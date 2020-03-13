890f17b25a0df0d7e6dddeab090b841af0e3d986
zhenweili <zhenweili_stu@163.com>
2020-03-13 14:30:06 +0800

    添加一个方法

---
+++ Git-test/src/com/atheima/git/FirstGit.java
@@ -0,0 +1,16 @@
+package com.atheima.git;
+
+public class FirstGit {
+    public static void main(String[] args) {
+        System.out.println("hello git project!");
+        System.out.println("hello git project!");
+        System.out.println("hello git project!");
+        System.out.println("hello git project!");
+        System.out.println("hello git project!");
+        System.out.println("hello git project!");
+        System.out.println("hello git project!");
+    }
+    public void test(){
+        System.out.println("加一个方法");
+    }
+}
